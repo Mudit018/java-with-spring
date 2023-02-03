1ea45f6cfca3f8f1969f4b5260d3dd203166a589
Mudit018 <agarwalmudit018@gmail.com>
2023-02-03 14:02:58 +0530

    loosely coupled classes with ApplicationContext, ClassPathXmlApplicationContext, setter injection and without annotation

--- src/main/java/com/practice/Football.java
+++ src/main/java/com/practice/Football.java
@@ -8,11 +8,11 @@ public class Football {
     public static void main(String[] args) {
 //        Footballers footballers = new Footballers(new Player1(), new Player2());
 
-//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("football.xml");
-//        Footballers footballers = applicationContext.getBean("footballers", Footballers.class);
-
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("football.xml");
         Footballers footballers = applicationContext.getBean("footballers", Footballers.class);
+
+//        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+//        Footballers footballers = applicationContext.getBean("footballers", Footballers.class);
 
         footballers.playerPlay();
     }
